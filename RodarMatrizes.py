e7f63889abc77baf4c82b1fdb3a2fc68c4fc9c69
Joaof14 <joao-pedro032011@hotmail.com>
2024-02-19 20:21:01 -0300

    ''

--- RodarMatrizes.py
+++ RodarMatrizes.py
@@ -36,7 +36,7 @@ acels = {'Nenhuma': metodo_da_potencia,
 
 
 
-caminho = 'matrizes/slot06'
+caminho = 'matrizes/slot07'
 arquivos_na_pasta = glob.glob(os.path.join(caminho, '*'))
 
 for arquivo in arquivos_na_pasta:
@@ -72,7 +72,7 @@ for arquivo in arquivos_na_pasta:
                 graf.savefig(arquivo+'.png')
 
         except:
-            coluna_tempo('erro')
+            coluna_tempo.append('erro')
             coluna_ite.append('erro')
             coluna_autovalor.append('erro')
             coluna_erro.append('erro')
@@ -92,6 +92,6 @@ dados = {
 
 df1 = pd.DataFrame(dados)
 
-df1['Matriz'] = df1['Matriz'].str.lstrip('matrizes/slot06')
+df1['Matriz'] = df1['Matriz'].str.lstrip('matrizes/slot07')
 
-df1.to_excel('resultados/resultados_slot06.xlsx')
+df1.to_excel('resultados/resultados_slot07.xlsx')

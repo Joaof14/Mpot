9576c8ae7537bc0fac236710662fe91b5370b07e
Joaof14 <joao-pedro032011@hotmail.com>
2024-02-19 22:20:50 -0300

    ''

--- RodarMatrizes.py
+++ RodarMatrizes.py
@@ -36,7 +36,7 @@ acels = {'Nenhuma': metodo_da_potencia,
 
 
 
-caminho = 'matrizes/slot09'
+caminho = 'matrizes/slot10'
 arquivos_na_pasta = glob.glob(os.path.join(caminho, '*'))
 
 for arquivo in arquivos_na_pasta:
@@ -92,6 +92,6 @@ dados = {
 
 df1 = pd.DataFrame(dados)
 
-df1['Matriz'] = df1['Matriz'].str.lstrip('matrizes/slot09')
+df1['Matriz'] = df1['Matriz'].str.lstrip('matrizes/slot10')
 
-df1.to_excel('resultados/resultados_slot09.xlsx')
+df1.to_excel('resultados/resultados_slot10.xlsx')
